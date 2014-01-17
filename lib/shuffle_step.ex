57acb737f548f749cba4284948377e746deb88dc
Augie De Blieck Jr <augiedb@gmail.com>
2014-01-16 22:54:44 -0500

    Add test -- is_a_match

--- lib/shuffle_step.ex
+++ lib/shuffle_step.ex
@@ -14,8 +14,16 @@ defmodule Deck do
     do: Card.new rank: rank, suit: suit, points: init_points(rank)
   end
 
+  def init_points(points) when points in 2..10, do: points
+  def init_points(points) when points == 'Ace', do: 1
+  def init_points(_), do: 10 
+
   def shuffle(deck) do
     deck |> Enum.shuffle
+  end
+
+  def is_a_match(card1, card2) do
+    (card1.suit == card2.suit) or (card1.rank == card2.rank)
   end
 
 end
@@ -29,11 +37,20 @@ defmodule ShuffleStep do
     ShuffleStep.Supervisor.start_link
   end
 
-  def count do
+  def run do
     deck = Deck.create()
+    [first_card | rest ] = deck
+    total_matches = count_matches(first_card, rest, 0)
+    IO.puts "Total matches: #{total_matches}"
+  end
 
+  def count_matches(_card, [head|[]], acc) do
+    acc
+  end
 
-
+  def count_matches(card1, [head|tail], acc) do
+    [card2, rest]  = head
+    count_matches(card2, rest, acc + Deck.is_a_match(card1, card2))
   end
 
 
--- test/shuffle_step_test.exs
+++ test/shuffle_step_test.exs
@@ -1,7 +1,13 @@
 defmodule ShuffleStepTest do
   use ExUnit.Case
 
-  test "the truth" do
-    assert(true)
+  test "Card Matches Work" do
+    c1 = Card.new rank: 'Jack', suit: 'Clubs',  points: 10
+    c2 = Card.new rank: 5,      suit: 'Hearts', points: 5
+    match = Card.new rank: 'Jack', suit: 'Hearts', points: 10
+    
+    assert(Deck.is_a_match(c1, c2) == false)
+    assert(Deck.is_a_match(c1, match) == true)
+    assert(Deck.is_a_match(c2, match) == true)
   end
 end
